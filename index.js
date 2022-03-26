ae24da79fab668ca75cbf9f3964d45e9f98572d6
Minhazur Rahman <rafi.rahman03@yahoo.com>
2022-03-26 19:54:30 +0600

    server committed

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json("Welcome to test server for ci cd");
+})
+
+app.listen(5000, () => {
+    console.log("Server is running");
+})
